7559610fc067648af707426faba24524a4785d60
Bruno <brunoalmeida0805@gmail.com>
2023-07-10 22:37:05 -0300

    focus timer em desenvolvimento

---
+++ main.js
@@ -0,0 +1,12 @@
+const buttonSoundForest = document.querySelector(".sound-forest");
+const buttonSoundRain = document.querySelector(".sound-rain");
+const buttonSoundCoffeeShop = document.querySelector(".sound-coffee-shop");
+const buttonSoundFireplace = document.querySelector(".sound-fireplace");
+
+buttonSoundForest.addEventListener("click", function () {});
+
+buttonSoundRain.addEventListener("click", function () {});
+
+buttonSoundCoffeeShop.addEventListener("click", function () {});
+
+buttonSoundFireplace.addEventListener("click", function () {});
